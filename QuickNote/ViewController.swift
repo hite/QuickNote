b26dbca624506c9b07771045145dd5ec7ce3be7e
hite <iamhite@gmail.com>
2021-06-19 15:38:04 +0800

    Initial Commit

---
+++ QuickNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickNote
+//
+//  Created by liang on 2021/6/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
